3e396cdc7c64bf65dcacc20924e6aee83e2042fc
Cintra, Guilherme <cinguilherme@gmail.com>
2018-07-01 19:05:26 -0300

    cirando container princiapl.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 
+import MainPage from './mainpage/MainPage';
+
 class App extends Component {
 
     render(){
         return(
             <div className="container">
-                new component App para lista telefonica
+                <MainPage />
             </div>
         )
     }
---
+++ src/components/mainpage/MainPage.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+import './MainPage.css';
+
+class MainPage extends Component{
+
+
+
+    render(){
+        return(
+            <div className="container MainPageContainer">
+                <div className="Title">title</div>
+                <div className="ListaTele">lista</div>
+                <div className="Botoes">buttons</div>
+            </div>
+        )
+    }
+}
+
+export default MainPage;
